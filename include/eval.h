d085342e764c07f37b5c4c06242503bfb87c1004
Kelechi Duru <75586620+KELECHIDAVIS@users.noreply.github.com>
2025-12-29 17:25:25 -0800

    Rebuilt builds for tournaments

--- include/eval.h
+++ include/eval.h
@@ -1,3 +1,16 @@
+// #ifndef EVAL_H
+// #define EVAL_H
+
+// #include "board.h"
+
+// // for the simple eval function:
+
+// extern int evaluate(const Board *board);
+
+// extern void init_pesto_tables();
+
+// #endif // EVAL_H
+
 #ifndef EVAL_H
 #define EVAL_H
 
@@ -5,8 +18,6 @@
 
 // for the simple eval function:
 
-extern int evaluate(const Board *board);
-
-extern void init_pesto_tables();
+extern double evaluate(const Board *board);
 
 #endif // EVAL_H
